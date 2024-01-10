fec818d484cfc3cb6d199cb4e76e7a481f13861c
EstherUduma <estheruduma23@gmail.com>
2024-01-10 05:51:08 -0800

    Implement interactive console with basic commands (quit, EOF) for user-friendly program interaction.

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Console module for the command interpreter
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand class for the command interpreter
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        EOF command to exit the program
+        """
+        print("")
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
