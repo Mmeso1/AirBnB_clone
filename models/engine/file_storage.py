cfa02b8a772097f283f2e6d12c8d54853ef12215
Mmeso1 <anisiubammesoma8@gmail.com>
2024-01-13 03:28:50 -0800

    Some fixes in file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,11 @@ This module defines the FileStorage
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -19,23 +24,23 @@ class FileStorage:
         """
         Returns the dictionary of all stored objects
         """
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """
         Adds a new object instance to the storage.
         """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """
         Serializes the stored objects to a JSON file
         """
         obj_dict = {}
-        for obj in FileStorage.__objects:
-            obj_dict[obj] = FileStorage.__objects[obj].to_dict()
-        with open(FileStorage.__file_path, mode="w") as file:
+        for obj in self.__objects:
+            obj_dict[obj] = self.__objects[obj].to_dict()
+        with open(self.__file_path, mode="w") as file:
             json.dump(obj_dict, file)
 
     def reload(self):
@@ -43,11 +48,11 @@ class FileStorage:
         Deserializes objects from the JSON file back into the storage
         """
         try:
-            with open(FileStorage.__file_path, 'r') as file:
+            with open(self.__file_path, 'r') as file:
                 obj_dict = json.load(file)
                 for key, value in obj_dict.items():
                     class_name, obj_id = key.split('.')
                     obj_instance = eval(class_name)(**value)
-                    FileStorage.__objects[key] = obj_instance
+                    self.__objects[key] = obj_instance
         except FileNotFoundError:
             pass
